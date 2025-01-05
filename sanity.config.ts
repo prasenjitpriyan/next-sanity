86f6f43ec242bc9930351dbbb1fe934d054245ab
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-05 19:37:45 +0530

    Schemas added

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
+import schemas from './sanity/schemas'
 
 const config = defineConfig({
   projectId: 'zf5e749w',
@@ -7,7 +8,8 @@ const config = defineConfig({
   title: 'My Personal Website',
   apiVersion: '2025-01-05',
   basePath: '/admin',
-  plugins: [deskTool()]
+  plugins: [deskTool()],
+  schema: { types: schemas }
 })
 
 export default config
