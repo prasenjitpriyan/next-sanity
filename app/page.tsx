82f10be9c520b5d7c247f8c02faa34649e1c1274
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-05 20:09:02 +0530

    sanity utils added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { getProjects } from '@/sanity/sanity-utils'
 
-const Home = () => {
-  return <div>Home</div>
+const Home = async () => {
+  const projects = await getProjects()
+  console.log(projects)
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project._id}>{project.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Home
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: 'zf5e749w',
+  dataset: 'production',
+  apiVersion: '2025-01-05'
+}
+
+export default config
